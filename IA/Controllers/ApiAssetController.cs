e60b5492274fdfead49e224aca7a5d95f2149716
Mindaugas Poskus <mindaugas.poskus@gmail.com>
2021-02-05 07:49:50 +0200

    initial api controller for assets

---
+++ IA/Controllers/ApiAssetController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Services;
+using Types.DTO;
+
+namespace IA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiAssetController : ControllerBase
+    {
+        private readonly IAssetService _assetService;
+        public ApiAssetController(
+            IAssetService assetService)
+        {
+            _assetService = assetService;
+        }
+
+        [HttpGet]
+        public IEnumerable<AssetDto> Get()
+        {
+            return _assetService.GetAll();
+        }
+    }
+}
